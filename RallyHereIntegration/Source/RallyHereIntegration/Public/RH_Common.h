3c5480045010bbc8a81435432131a7352f07c773
Chris Ranney <cranney@rallyhere.gg>
2024-03-21 18:22:08 -0400

    Add overridable version getters, reroute our version hooks to use them

--- RallyHereIntegration/Source/RallyHereIntegration/Public/RH_Common.h
+++ RallyHereIntegration/Source/RallyHereIntegration/Public/RH_Common.h
@@ -691,4 +691,19 @@ static bool RH_BreakApartURL(const FString& URL, const FString& BaseURL, FString
 	return true;
 }
 
+namespace RH_VersionStrings
+{
+	/** @brief Retrieves a version number to use to indicate a build version */
+	RALLYHEREINTEGRATION_API FString GetBuildVersion();
+	/** @brief Retrieves a version number to use for session compatibility checks, not necessarily same as build version */
+	RALLYHEREINTEGRATION_API FString GetVersionForSession();
+	
+	// override delegates for version retrieval, otherwise uses engine version
+	DECLARE_DELEGATE_RetVal(FString, FRallyHereVersionDelegate);
+	/** @brief Override delegate for overriding behavior of GetBuildVersion() */
+	extern FRallyHereVersionDelegate OnGetBuildVersion;
+	/** @brief Override delegate for overriding behavior of GetVersionForSession() */
+	extern FRallyHereVersionDelegate OnGetVersionForSession;
+};
+
 /** @} */
