99c5f514ba720cc4aa6432ada39d970dc90226c0
Chris Ranney <cranney@rallyhere.gg>
2024-04-24 12:53:53 -0400

    Fix char array deref (used to be a string)

--- RallyHereIntegration/Source/RallyHereGameHostProvider/Private/RH_GameHostProviderGHA.cpp
+++ RallyHereIntegration/Source/RallyHereGameHostProvider/Private/RH_GameHostProviderGHA.cpp
@@ -42,7 +42,7 @@ FRH_GameHostProviderGHA::FRH_GameHostProviderGHA(const FString& Commandline)
 		FTCHARToUTF8 InstanceName((const TCHAR*)*FApp::GetInstanceId().ToString());
 		FTCHARToUTF8 LaunchDir((const TCHAR*)*FPaths::LaunchDir());
 		FTCHARToUTF8 ProjectName((const TCHAR*)FApp::GetProjectName());
-		FTCHARToUTF8 BuildVersion((const TCHAR*)*FApp::GetBuildVersion());
+		FTCHARToUTF8 BuildVersion((const TCHAR*)FApp::GetBuildVersion());
 
 		stats.name = (ANSICHAR*)InstanceName.Get();
 		provided.set_name = true;
