2e10067834216f162261b2c7df8360b70ceb34fb
Chris Ranney <cranney@rallyhere.gg>
2024-10-31 11:36:30 -0400

    Potential fix for showing debug tool after all widgets were removed from the viewport

--- UnrealImGui/Source/ImGui/Private/ImGuiModuleManager.cpp
+++ UnrealImGui/Source/ImGui/Private/ImGuiModuleManager.cpp
@@ -280,6 +280,18 @@ void FImGuiModuleManager::SetViewportWidgetVisibility(UGameViewportClient* GameV
 	auto Widget = Widgets.FindByPredicate([GameViewport](const auto& Widget) { return Widget.IsValid() && Widget.Pin()->GetGameViewport().Get() == GameViewport; });
 	if (Widget && Widget->IsValid())
 	{
-		return Widget->Pin()->SetVisibility(bVisible ? EVisibility::Visible : EVisibility::Collapsed);
+		//$$ BEGIN check if the widget still has a valid parent, if it does not and we try to set visibility to true, re-add it to the game viewport if possible
+		auto SharedWidget = Widget->Pin();
+		if (!SharedWidget->IsParentValid())
+		{
+			auto GameViewport = SharedWidget->GetGameViewport();
+			if (GameViewport.IsValid())
+			{
+				GameViewport->RemoveViewportWidgetContent(SharedWidget.ToSharedRef());
+				GameViewport->AddViewportWidgetContent(SharedWidget.ToSharedRef(), IMGUI_WIDGET_Z_ORDER);
+			}
+		}
+		return SharedWidget->SetVisibility(bVisible ? EVisibility::Visible : EVisibility::Collapsed);
+		//$$ END
 	}
 }
