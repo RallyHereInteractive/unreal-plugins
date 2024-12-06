f0d804fa075adc8cd50558db968b19848bfe4e5c
cskellen <cskellen@hirezstudios.com>
2024-12-06 10:52:17 -0500

    Relocate where doxygen files are stored and update bash script

--- Documentation/generate.bash
+++ Documentation/generate.bash
@@ -9,11 +9,11 @@ doxygen doxygen.cfg
 
 moxygen --groups --html-anchors --templates ./templates --output %s.md xml
 
-rm -rf md
-mkdir md
-mkdir md/models
-mv ./RHAPI_*.md ./md/models/
-mv ./*.md ./md/
+rm -rf md/Reference
+mkdir md/Reference
+mkdir md/Reference/models
+mv ./RHAPI_*.md ./md/Reference/models/
+mv ./*.md ./md/Reference/
 
 sed -i 's/(RHAPI_/(models\/RHAPI_/' ./md/*.md
 
---
+++ Documentation/md/index.md
@@ -0,0 +1,5 @@
+# Welcome to RallyHere
+
+RallyHere is a platform for backend game services, including authentication, matchmaking, inventory, purchasing, and much more.
+
+If you are an Unreal Engine developer, you should start with the [RallyHere Unreal Integration QuickStart Guide](./Integration/02%20-%20Unreal%20Integration/Unreal%20Quickstart.md)
